4ed68327d731f87f288f1ea38213b53414f43e6d
Black Miner <andreiraileanu@Andreis-iMac.local>
2014-05-27 12:20:46 +0300

    Update

--- ghash.php
+++ ghash.php
@@ -6,8 +6,5 @@
 	$api = new cexapi("ionutrai", "Ub2iMQkOPn3WWWf6kWxceFGc", "c3ZAhjFLR16nExRqumayqF40onk");
 	
 	// Test some API Methods
-	echo "Ticker:<pre>", json_encode($api->ticker()), "</pre>";
-	echo "Order Book:<pre>", json_encode($api->order_book()), "</pre>";
-	echo "Balance:<pre>", json_encode($api->balance()), "</pre>";
-	echo "Open Orders:<pre>", json_encode($api->open_orders()), "</pre>";
+	echo "Hashrate:<pre>", json_encode($api->hashrate()), "</pre>";
 ?>
